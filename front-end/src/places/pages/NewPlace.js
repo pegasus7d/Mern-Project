3e38c4095ab3ef66c5f7b244c511e1b1747d418c
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-08 02:06:27 +0530

    added validity to the title and description

--- front-end/src/places/pages/NewPlace.js
+++ front-end/src/places/pages/NewPlace.js
@@ -1,8 +1,30 @@
-const NewPlace=()=>{
-    return (
-        <div>
-            New Places work!!
-        </div>
-    );
-}
+import Input from "../../shared/FormElemets/Input";
+import "./NewPlace.css";
+import {  VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import { useCallback } from "react";
+const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+  return (
+    <form className="place-form">
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title {at least 5 characters}"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_REQUIRE(),VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description"
+        onInput={descriptionInputHandler}
+      />
+    </form>
+  );
+};
 export default NewPlace;
