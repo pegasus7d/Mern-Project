a729085ddc600d9e2bc4dea17bbfb6d978c01eb3
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-08 03:32:20 +0530

    address and button added

--- front-end/src/places/pages/NewPlace.js
+++ front-end/src/places/pages/NewPlace.js
@@ -1,12 +1,71 @@
 import Input from "../../shared/FormElemets/Input";
 import "./NewPlace.css";
-import {  VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
-import { useCallback } from "react";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import { useCallback, useReducer } from "react";
+import Button from "../../shared/FormElemets/Button";
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: {
+            value: action.value,
+            isValid: action.isValid,
+          },
+        },
+        isValid: formIsValid,
+      };
+
+    default:
+      return state;
+  }
+};
 const NewPlace = () => {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+  // eslint-disable-next-line
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+    },
+    // eslint-disable-next-line
+    inputs: {
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const InputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
+  const placeSubmitHandler= event =>{
+    event.preventDefault();
+    console.log(formState.inputs)
+  };
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -14,16 +73,27 @@ const NewPlace = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title {at least 5 characters}"
-        onInput={titleInputHandler}
+        onInput={InputHandler}
       />
       <Input
         id="description"
         element="textarea"
         label="Description"
-        validators={[VALIDATOR_REQUIRE(),VALIDATOR_MINLENGTH(5)]}
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description"
-        onInput={descriptionInputHandler}
+        onInput={InputHandler}
       />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid description"
+        onInput={InputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD Place
+      </Button>
     </form>
   );
 };
