97145099b210c3b3f17ee6dce4bbe5be1f220855
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-12 02:23:12 +0530

    added validity to the routes

--- back-end/routes/users-routes.js
+++ back-end/routes/users-routes.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 
-const usersController = require('../Controllers/users-controllers');
+const { check } = require("express-validator");
+
+const usersController = require("../Controllers/users-controllers");
 
 const router = express.Router();
 
-router.get('/', usersController.getUsers);
+router.get("/", usersController.getUsers);
 
-router.post('/signup', usersController.signup);
+router.post("/signup",[
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail() // Test@test.com => test@test.com
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ], usersController.signup);
 
-router.post('/login', usersController.login);
+router.post("/login", usersController.login);
 
 module.exports = router;
