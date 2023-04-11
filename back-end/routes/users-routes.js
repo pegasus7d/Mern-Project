665b9001159406166cd59e883524806cfd72c23b
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-12 01:44:09 +0530

    added users-routes and patch and delete in places

--- back-end/routes/users-routes.js
+++ back-end/routes/users-routes.js
@@ -1,1 +1,13 @@
-const express=require('express');
+const express = require('express');
+
+const usersController = require('../Controllers/users-controllers');
+
+const router = express.Router();
+
+router.get('/', usersController.getUsers);
+
+router.post('/signup', usersController.signup);
+
+router.post('/login', usersController.login);
+
+module.exports = router;
