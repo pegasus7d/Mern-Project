41faabd5eb1ee1ed56e0053c560dea9fc12389f1
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-13 03:45:37 +0530

    connected the backend with the frontend for signup and login page

--- back-end/app.js
+++ back-end/app.js
@@ -8,6 +8,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
 app.use("/api/places", placesRoutes); // => /api/places...
 app.use("/api/users", usersRoutes);
 
@@ -25,7 +37,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect("mongodb+srv://<username>:<password>@cluster0.whplxzw.mongodb.net/places?retryWrites=true&w=majority")
+  .connect("mongodb+srv://<username>:<password>@cluster0.whplxzw.mongodb.net/mern?retryWrites=true&w=majority")
   .then(() => {
 
   })
