1a53aa4a35ab504c10c67b2426923447a6092c6c
pegasus7d <debayanbiswas1111@gmail.com>
2023-04-15 17:08:13 +0530

    project complete

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import Users from "./user/pages/Users";
+// import Users from "./user/pages/Users";
 import { BrowserRouter as Router } from "react-router-dom";
-import NewPlace from "./places/pages/NewPlace";
+// import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
-import UserPlaces from "./places/pages/UserPlaces";
-import UpdatePlace from "./places/pages/UpdatePlace";
-import Auth from "./user/pages/Auth";
+// import UserPlaces from "./places/pages/UserPlaces";
+// import UpdatePlace from "./places/pages/UpdatePlace";
+// import Auth from "./user/pages/Auth";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
-
-
+import React from "react";
+import { Suspense } from "react";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+const Users=React.lazy(()=>import("./user/pages/Users"))
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
 
 
 const App = () => {
@@ -53,7 +59,11 @@ const App = () => {
     >
       <Router>
         <MainNavigation />
-        <main>{routes}</main>
+        <main><Suspense fallback={
+          <div className="center">
+          <LoadingSpinner />
+        </div>
+        }>{routes}</Suspense></main>
       </Router>
     </AuthContext.Provider>
   );
